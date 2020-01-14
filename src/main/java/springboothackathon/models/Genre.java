b528ec420d6770228ae9bd50698aa6b49bbf7361
Wheeler Weise <wheeler.weise@gmail.com>
2020-01-14 15:23:35 -0600

    progress

---
+++ src/main/java/springboothackathon/models/Genre.java
@@ -0,0 +1,36 @@
+package springboothackathon.models;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="GENRES")
+public class Genre {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long value)
+    {
+        id = value;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String value)
+    {
+        name = value;
+    }
+}
