c8b6a17b20ad4d69f3192c315da8fbb9dcdec0f4
PaulBollinger <paul.bollinger@daugherty.com>
2020-01-14 10:10:09 -0600

    Have two working cucumber API tests

--- src/test/java/cucumbertests/stepdefs/StepDefinitions.java
+++ src/test/java/cucumbertests/stepdefs/StepDefinitions.java
@@ -1,22 +1,62 @@
 package cucumbertests.stepdefs;
 
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import cucumbertests.SpringIntegrationTest;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
+import org.springframework.boot.SpringApplication;
+import springboothackathon.Application;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
 public class StepDefinitions extends SpringIntegrationTest {
+
+    int resCode;
+    URL url;
+    StringBuffer response;
+
+    @Given("^the server is running$")
+    public void the_server_is_running()  {
+        SpringApplication.run(Application.class);
+    }
+
     @When("^the client calls home controller$")
-    public void getStuff() throws IOException {
-        URL url = new URL("http://localhost:8080/home");
+    public void the_client_calls_home_controller() throws IOException {
+        url = new URL("http://localhost:8080/home");
+        String readLine = null;
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
-        con.setDoOutput(true);
-        DataOutputStream out = new DataOutputStream(con.getOutputStream());
-        out.flush();
-        out.close();
+        resCode = con.getResponseCode();
+
+        if (resCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            response = new StringBuffer();
+            while ((readLine = in .readLine()) != null) {
+                response.append(readLine);
+            } in .close();
+        } else {
+            System.out.println("GET NOT WORKED");
+        }
+    }
+
+    @Then("^the client gets a success response$")
+    public void the_client_gets_a_success_response() {
+        assertThat(resCode).isEqualTo(200);
+    }
+
+    @Then("^the client receives the response string Greetings from Spring Boot!$")
+    public void theClientReceivesTheResponseStringGreetingsFromSpringBoot() {
+        assertThat(response.toString()).isEqualTo("Greetings from Spring Boot!");
     }
 }
