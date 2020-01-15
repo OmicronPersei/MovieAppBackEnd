3aab2d0d7a8973dfba5aaab71226932cd2ae5cf7
PaulBollinger <paul.bollinger@daugherty.com>
2020-01-15 09:05:35 -0600

    Added Edge assertHomePage Test

---
+++ src/test/java/seleniumtests/homepageasserttitle/EdgeHomePageTitleTest.java
@@ -0,0 +1,33 @@
+package seleniumtests.homepageasserttitle;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class EdgeHomePageTitleTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        driver = new EdgeDriver();
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void assertTitle() {
+        driver.get("https://spring-boot-hackathon-265015.appspot.com/");
+        assertThat(driver.getTitle(), is("Movie Lookup"));
+    }
+}
