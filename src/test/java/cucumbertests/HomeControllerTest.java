2f71c94fb5ae71aa527d862d15100c3cd28d56ec
McKim <AAM1022@daugherty.com>
2020-01-15 08:43:53 -0600

    pulled in endpoints branch-AM

--- src/test/java/cucumbertests/HomeControllerTest.java
+++ src/test/java/cucumbertests/HomeControllerTest.java
@@ -6,7 +6,8 @@ import org.junit.runner.RunWith;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features")
+@CucumberOptions(features = "src/test/resources/features",
+    glue = "cucumbertests.stepdefs")
 public class HomeControllerTest {
 
 }
